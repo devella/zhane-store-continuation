08885416e60e122a52992b19921230df942b7237
samson <malonee20@gmail.com>
2020-05-14 22:20:00 +0300

    updating app with reslect and replacing cart selector with memorize selectors

--- src/components/dropdownshopping-cart/Dropdown-cart.js
+++ src/components/dropdownshopping-cart/Dropdown-cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import  { selectCartItems} from '../../redux/cart/CartSelector'
 import './Dropdown-cart.scss';
 
 const DropdownCart = ({ cartItems }) => (
@@ -15,8 +16,8 @@ const DropdownCart = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(DropdownCart);
